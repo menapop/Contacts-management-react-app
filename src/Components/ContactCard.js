6598a180c156381cfcf899a6deb9124429a051ba
menapop <menamorcosfci5@gmail.com>
2022-06-13 20:24:59 +0200

    Confirmation on delete

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,9 +1,31 @@
 
 import {Link} from 'react-router-dom'
 import user from '../images/user.jpg' ;
-
+import { confirmAlert } from 'react-confirm-alert';
 const ContactCard = (props) => {
   const {id ,name ,email} = props.contact ;
+
+const onDelete = (name,id) => {
+  confirmAlert({
+    customUI: ({ onClose }) => {
+      return (
+        <div className='custom-ui'>
+          <h1>DELETE CONFIRMATION ?</h1>
+          <p>ARE YOU WANT TO DELETE {name.toUpperCase()} CONTACT?</p>
+          <button onClick={onClose}>No</button>
+          <button
+            onClick={() => {
+              props.deleteHandler(id)
+              onClose();
+            }}
+          >
+            Yes, Delete it!
+          </button>
+        </div>
+      );
+    }
+  });
+}
   return (
     <div className="item">
     <img className="ui avatar image" src={user} alt="user" />
@@ -16,7 +38,7 @@ const ContactCard = (props) => {
     <i
       className="trash alternate outline icon"
       style={{ color: "red", marginTop: "7px", marginLeft: "10px" }}
-      onClick={() => props.deleteHandler(id)}
+      onClick={() => onDelete(name,id)}
     ></i>
     <Link to={`/editContact/${id}`}>
       <i
